2e03106282cbc4162a6fd50f1bb59ee60bb4a275
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2024-10-23 14:14:14 +0530

    loans impl

---
+++ loans/src/main/java/com/microservice/loans/dto/LoansDto.java
@@ -0,0 +1,56 @@
+package com.microservice.loans.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Data
+@Schema(
+        name="Loans",
+        description = "Schema to hold Loans information"
+)
+public class LoansDto {
+
+    @NotEmpty(message = "Mobile Number cannot be empty or null")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    @Schema(
+            description = "Mobile number of customer",
+            example = "1414141414"
+    )
+    private String mobileNumber;
+
+    @NotEmpty(message = "Loan Number cannot be empty or null")
+    @Pattern(regexp = "(^$|[0-9]{12})", message = "Loan number must be 12 digits")
+    @Schema(
+            description = "Loan number of customer",
+            example = "101010101000"
+    )
+    private String loanNumber;
+
+    @NotEmpty(message = "Loan type cannot be null")
+    @Schema(
+            description = "Type of the loan", example = "Home Loan"
+    )
+    private String loanType;
+
+    @Positive(message = "Total loan amount should be greater than zero")
+    @Schema(
+            description = "Total loan amount", example = "100000"
+    )
+    private int totalLoan;
+
+    @PositiveOrZero(message = "Total loan amount paid should be equal or greater than zero")
+    @Schema(
+            description = "Total loan amount paid", example = "1000"
+    )
+    private int amountPaid;
+
+    @PositiveOrZero(message = "Total outstanding amount should be equal or greater than zero")
+    @Schema(
+            description = "Total outstanding amount against a loan", example = "99000"
+    )
+    private int outstandingAmount;
+}
---
+++ loans/src/main/java/com/microservice/loans/repo/LoansRepository.java
@@ -0,0 +1,13 @@
+package com.microservice.loans.repo;
+
+import com.microservice.loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
+}
