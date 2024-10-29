ae66e4b07a9db047c39b155db3f41f9dfc8ce452
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2024-10-29 13:38:04 +0530

    test commit

---
+++ loans/src/main/java/com/microservice/loans/LoansApplication.java
@@ -0,0 +1,36 @@
+package com.microservice.loans;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Bank microservice REST API Documentation",
+                description = "Banking System microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Prakash Anandakumar",
+                        email = "prakash.anandakumar@acentura.com",
+                        url = "https://start.spring.io/"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://start.spring.io/"
+                )
+        )
+)
+
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+
+}
