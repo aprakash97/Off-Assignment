7dd5be7001dcd7fcb13ebbd7b28b0c20f87ac1f4
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2024-11-01 14:46:45 +0530

    changed gateway API base

--- gatewayserver/src/main/java/com.microservice.gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com.microservice.gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package com.microservice.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,21 @@ public class GatewayserverApplication {
         SpringApplication.run(GatewayserverApplication.class, args);
     }
 
+//    Authentication should be checked
+    @Bean
+    public RouteLocator bankRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/bankapp/accounts/**")
+                        .filters(f -> f.rewritePath("/bankapp/accounts/(?<segment>.*)","/${segment}"))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/bankapp/loans/**")
+                        .filters(f -> f.rewritePath("/bankapp/loans/(?<segment>.*)","/${segment}"))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/bankapp/authentication/**")
+                        .filters(f -> f.rewritePath("/bankapp/authentication/(?<segment>.*)","/${segment}"))
+                        .uri("lb://AUTHENTICATION")).build();
+    }
 }
